1ae92c23238408ff4b173baa819e66e065415a2b
shehan-kv <kvsm066@gmail.com>
2026-03-18 18:11:33 +0530

    chore: initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<svelte:head>
+    <title>Happy Birthday, Hashiki 🎉🥳 !!</title>
+</svelte:head>
+
+<audio autoplay loop class="hidden">
+    <source src="/background.mp3" type="audio/mpeg">
+    Your browser does not support the audio element.
+</audio>
+
+<div class="h-screen flex items-center justify-center snap-start" style="background-color: #fee6e2;">
+    <p style="color: #45130a;" class="text-4xl">Happy Birthday, Hashiki 🎉🥳 !!</p>
+</div>
+
+<div class="h-screen flex items-center justify-center snap-start">
+    <p>PS: I'm sorry I couldn't send a real gift 🥺</p>
+</div>
+
+<div class="h-screen flex items-center justify-center snap-start" style="background-color: #ffead5;">
+    <div class="space-y-2" style="color: #430f07;">
+        <p>I don’t think I say this enough, but... </p>
+        <p class="text-4xl">You're that rare soul who lights up the world 
+        </p>
+        <p class="text-4xl">
+            in a way most people only dream of finding 😍
+        </p>    
+    </div>
+</div>
+
+<div class="h-screen flex items-center justify-center snap-start" style="background-color: #f8edeb;">
+    <div class="space-y-2" style="color: #371d18;">
+        <p >What I love most is just… you</p>
+        <p class="text-4xl">Honest, down-to-earth, and so deeply kind ✨</p>
+    </div>
+</div>
+
+<div class="h-screen flex items-center justify-center snap-start" style="background-color: #f3f6f6;">
+    <div class="space-y-2 text-center" style="color: #202d23;">
+        <p class="text-4xl">I’m really grateful that life somehow</p>
+        <p class="text-4xl">let me get to know you</p>
+    </div>
+</div>
+
+<div class="h-screen flex items-center justify-center snap-start" style="background-color: #fee6e2;">
+        <p class="text-4xl" style="color: #45130a;">Here’s to another year of you... 💖</p>
+</div>
+
+<div class="h-screen flex items-center justify-center snap-start" style="background-color: #fff6ed;">
+        <p class="text-4xl" style="color: #430f07;">...shining as brilliantly as you do 🌟</p>
+</div>
+
+<div class="h-screen flex items-center justify-center snap-start" style="background-color: #fee6e2;">
+        <p class="text-4xl" style="color: #45130a;">...and reaching every goal 🎊</p>
+</div>
+
+<div class="h-screen flex items-center justify-center snap-start" style="background-color: #f8edeb;">
+        <p class="text-4xl" style="color: #371d18;">...you’ve ever set your heart on ❤️</p>
+</div>
+
+<div class="h-screen flex items-center justify-center snap-start" style="background-color: #f8fbfb;">
+        <p class="text-4xl" style="color: #070e0a;">You deserve all of it. And maybe a little extra too 🥳</p>
+</div>
